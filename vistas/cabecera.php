22014e369c8369edbfbf66e46f252e490b0e2130
Paul Lugo <paulugortega@gmail.com>
2022-07-13 14:38:39 -0600

    favicon y validacion de asunto solucionados

--- vistas/cabecera.php
+++ vistas/cabecera.php
@@ -9,8 +9,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Denuncia Ciudadana</title>
+    <link rel="shortcut icon" href="assets/favicon.png">
     <link rel="stylesheet" href="css/resetearvalores.css">
-    <link rel="stylesheet" href="css/estilodenuncia.css?version3">
+    <link rel="stylesheet" href="css/estilodenuncia.css?version5">
     <link rel="stylesheet" href="css/estiloestatus.css">
     <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
