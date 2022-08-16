dd29d730dfa2a91bd24bded590b250086db1f24b
Paul Lugo <paulugortega@gmail.com>
2022-08-16 15:43:07 -0600

    Segundo avance en seguimiento denuncia anónima

--- servidor/registrarseguimientoda.php
+++ servidor/registrarseguimientoda.php
@@ -1,37 +1,32 @@
 <?php
     include 'conexion.php';
+    session_start();
     
-    
-    $nombre = $_POST['nombre'];
+    $sesionAsesor = $_SESSION['id_usuario'];
+    $idDenuncia = $_POST['id'];
     $estatus = $_POST['estatus'];
     $nota = $_POST['nota'];
     
+    echo $idDenuncia;
   
     if($dbh!=null){  //Si hay una conexión esté establecida.
          
         //Se realiza el registro con sentencias preparadas.
-        $stmt = $dbh-> prepare("UPDATE `servidor publico` 
-        SET nombre = ?, edad = ?, sexo = ?, telefono = ?, 
-        correo = ?, direccion = ?, area = ?, clave = ?, rol_usuario = ? 
-        WHERE id_usuario = ?");
-        $stmt->bindParam(1,$nombre);
-        $stmt->bindParam(2,$edad);
-        $stmt->bindParam(3,$sexo);
-        $stmt->bindParam(4,$telefono);
-        $stmt->bindParam(5,$correo);
-        $stmt->bindParam(6,$direccion);
-        $stmt->bindParam(7,$area);
-        $stmt->bindParam(8,$clave);
-        $stmt->bindParam(9,$rol);
-        $stmt->bindParam(10,$idUsuario);
+        $stmt = $dbh-> prepare("UPDATE `estatus de denuncia` 
+        SET id_asesor = ?, estatus = ?, nota = ? 
+        WHERE id_denuncia_a = ?");
+        $stmt->bindParam(1,$sesionAsesor);
+        $stmt->bindParam(2,$estatus);
+        $stmt->bindParam(3,$nota);
+        $stmt->bindParam(4,$idDenuncia);
         $stmt->execute();
             
         $dbh=null; //Para cerrar la conexión a base de datos. 
 
-        echo '<script language="javascript">
+        /*echo '<script language="javascript">
             alert("Se ha realizado la modificación con éxito");
             location.href="../usuarios.php";
-            </script>';
+            </script>';*/
         } else{
             echo '<script language="javascript">
                 alert("Error al conectar la base de datos");
