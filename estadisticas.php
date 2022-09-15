b8d1978580dacf6294edae6bb601ed5ca9a1b8d1
Paul Lugo <paulugortega@gmail.com>
2022-09-15 14:19:37 -0600

    Estilos de estadisticas

--- estadisticas.php
+++ estadisticas.php
@@ -4,12 +4,39 @@
         <canvas id="grafica1">
         
         </canvas>
+        <div class="lista-tipo">
+                <p>1 Denuncia anónima</p>
+                <p>2 Denuncia ciudadana</p>
+                <p>3 Denuncia servidor público</p>
+        </div>
     </div>
 
     <div class="contenedor-canvas-2">
         <canvas id="grafica2">
         
         </canvas>
+        <div class="lista-tipo">
+                <p>1 Abuso de autoridad</p>
+                <p>2 Acoso y Hostigamiento</p>
+                <p>3 Soborno para algún trámite o servicio</p>
+                <p>4 Incumplimiento o mal uso de un programa o acción social</p>
+                <p>5 Trato irrespetuoso y mala conducta</p>
+                <p>6 Servidor público autorisa, solicita o realiza actos para su beneficio</p>
+                <p>7 Solicitud de documentos o dinero adicional para la expedición de documentos</p>
+                <p>8 Otro</p>
+        </div>
+    </div>
+
+    <div class="contenedor-canvas-3">
+        <canvas id="grafica3">
+        
+        </canvas>
+        <div class="lista-tipo">
+                <p>1 En espera</p>
+                <p>2 En proceso</p>
+                <p>3 Cancelado</p>
+                <p>4 No aplica</p>
+        </div>
     </div>
 </div>
 
--- js/graficas.js
+++ js/graficas.js
@@ -3,11 +3,11 @@
     var chart = new Chart(grafica1, {
         type: "bar",
         data: {
-            labels:["Denuncia ciudadana","Denuncia anónima","Denuncia servidor público"],
+            labels:[1,2,3],
             datasets:[
                 {
-                    label:"Estadísticas de denuncia ciudadana",
-                    backgroundColor:"rgb(77,77,77)",
+                    label:"Tipo de denuncia ciudadana",
+                    backgroundColor:"rgb(255,165,0)",
                     borderColor:"rgb(106,28,50)",
                     data:[70,30,40] 
                 }
@@ -20,13 +20,30 @@
     var chart = new Chart(grafica2, {
         type: "bar",
         data: {
-            labels:["Acoso","Soborno","Malos manejos"],
+            labels:[1,2,3,4,5,6,7,8],
             datasets:[
                 {
-                    label:"Estadísticas de denuncia ciudadana",
-                    backgroundColor:"rgb(77,77,77)",
+                    label:"Asunto de denuncia",
+                    backgroundColor:"rgb(255,165,0)",
                     borderColor:"rgb(106,28,50)",
-                    data:[12,23,6] 
+                    data:[12,23,6,10,5,24,16,7] 
+                }
+            ]
+        }
+    })
+
+    let grafica3 = document.getElementById("grafica3").getContext("2d");
+
+    var chart = new Chart(grafica3, {
+        type: "bar",
+        data: {
+            labels:[1,2,3,4],
+            datasets:[
+                {
+                    label:"Estatus de denuncia",
+                    backgroundColor:"rgb(255,165,0)",
+                    borderColor:"rgb(106,28,50)",
+                    data:[23,5,16,7] 
                 }
             ]
         }
