9fe204537677d7253b28fc3c0f5fc1c74d5fda44
Paul Lugo <paulugortega@gmail.com>
2022-10-14 18:35:01 -0600

    Index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
             <h1>Qué SÍ es una denuncia ciudadana</h1>
             <h3>Hechos que tú consideres irregulares, como faltas <br>
                 administrativas, que involucren a:</h3>
-            <h4>1. Servidores públicos federales en ejercicio de sus funciones, o;</h4>
+            <h4>1. Funcionarios municipales en ejercicio de sus funciones, o;</h4>
             <h4>2. Personas físicas o morales que:</h4>
             <p>
                 · Manejen o apliquen recursos públicos federales. <br>
